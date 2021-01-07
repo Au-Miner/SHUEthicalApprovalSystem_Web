5aea505251480932d16b89a1b003bce4a6727ae3
Au-Miner <358671982@qq.com>
2021-01-07 21:48:40 +0800

    wocaole

--- src/components/Homepage.vue
+++ src/components/Homepage.vue
@@ -1,4 +1,5 @@
-<template>
+
+<template >
   <div class="block">
     <div id="title_Start">
       <img id="picture01" src="../assets/shu_pic.png" />
@@ -8,91 +9,120 @@
       <el-button type="primary" id="button02">伦理常识</el-button>
       <el-button type="primary" id="button03">我要申报</el-button>
     </div>
-    <div id="carousel_map">
-      <el-carousel trigger="click" @blur="blurText"> 
-        <el-carousel-item v-for="item in imgList" :key="item.name">
-          <img :src="item.src" style="height:100%;width:100%;" alt="图片丢失了" :title="item.title" />
+
+    <!-- <div id="carousel_Map">
+      <el-carousel trigger="click">
+        <el-carousel-item v-for="item in imgList1" :key="item.name">
+          <img :src="item.src" style="height:100%;width:100%;" />
         </el-carousel-item>
+      </el-carousel>
+    </div>-->
+
+    <div id="knowledge_Ethics">
+      <el-carousel trigger="focus">
+        <div v-if="choice > 0">
+          <el-button
+            type="warning"
+            icon="el-icon-close"
+            id="knowledge_Ethics_Getback"
+            @click="clickEvent01"
+          ></el-button>
+          <el-carousel-item v-for="item in imgList2" :key="item.name">
+            <img :src="item.src" style="height:100%;width:100%;" />
+          </el-carousel-item>
+        </div>
       </el-carousel>
     </div>
   </div>
 </template>
 
 <script>
+// import func from '../vue-temp/vue-editor-bridge';
 export default {
   name: "Homepage",
   data() {
     return {
-      imgList: [
+      choice: 1,
+      imgList1: [
         {
           name: "lj",
           src: require("../assets/Carousel_Map_pic01.jpg"),
-          title: "这是wql.jpg"
+          title: "wqlsb.jpg"
         },
         {
           name: "logo",
           src: require("../assets/Carousel_Map_pic02.png"),
-          title: "这是czw.jpg"
+          title: "czwnb.jpg"
         },
         {
           name: "bg",
           src: require("../assets/Carousel_Map_pic03.png"),
-          title: "这是wql.jpg"
+          title: "3.jpg"
+        }
+      ],
+      imgList2: [
+        {
+          name: "lj",
+          src: require("../assets/knowledge_ethics_pic01.jpg"),
+          title: "4.jpg"
+        },
+        {
+          name: "logo",
+          src: require("../assets/knowledge_ethics_pic02.jpg"),
+          title: "5.jpg"
+        },
+        {
+          name: "bg",
+          src: require("../assets/knowledge_ethics_pic03.jpg"),
+          title: "6.jpg"
         }
       ]
     };
+  },
+  methods: {
+    clickEvent01: function() {
+      // this.choice = 0;
+      var wocao = document.getElementById("knowledge_Ethics");
+      wocao.style.animation="animation:mymove 5s infinite";
+        //  wocao.style.display="none";
+    }
   }
 };
 </script>
 
 
 
+
 <style>
-.el-carousel {
+#title_Start {
   position: absolute;
-  width: 1920px;
-  height: 812px;
-  top: 125px;
+  top: 0px;
   left: 0px;
-  z-index: -1;
+  width: 1920px;
+  height: 170px;
+  z-index: 2;
 }
-.el-carousel button {
+#title_Start:hover #background01 {
+  transition: all 0.2s ease-in;
+  width: 1920px;
+  height: 125px;
+  background-color: #245086;
+  background-image: linear-gradient(#245086, #245086);
   position: absolute;
-  top: 405px;
-  width: 80px;
-  height: 80px;
-  background-color: rgb(240, 137, 137);
-  opacity: 0.5;
-  font-size: 30px;
+  top: 0px;
+  left: 0px;
+  z-index: 0;
+  opacity: 1;
 }
-.el-carousel button:hover {
-  background-color: rgb(240, 137, 137);
-  opacity: 0.7;
-  font-size: 30px;
-}
-.el-carousel__item:nth-child(2n) {
-  background-color: #99a9bf;
-  height: 812px;
-}
-.el-carousel__item:nth-child(2n + 1) {
-  background-color: #d3dce6;
-  height: 812px;
-}
-</style>
-
-
-
-
-
-<style scoped>
 #background01 {
+  transition: all 0.2s ease-in;
   width: 1920px;
   height: 170px;
   background-image: linear-gradient(#245086, #698cb6 70%);
   position: absolute;
   top: 0px;
   left: 0px;
-  z-index: 0;
+  z-index: 1;
   opacity: 0.9;
   /* display: none; */
 }
@@ -102,7 +132,7 @@ export default {
   position: absolute;
   top: 0px;
   left: 15px;
-  z-index: 1;
+  z-index: 2;
 }
 #title_text01 {
   height: 40px;
@@ -112,40 +142,146 @@ export default {
   top: 45px;
   font-size: 40px;
   color: #fefefe;
+  z-index: 2;
 }
 #button01 {
   height: 40px;
   width: 140px;
   position: absolute;
   left: 1200px;
-  top: 65px;
+  top: 55px;
   font-size: 20px;
   color: #fefefe;
   background: rgba(0, 0, 0, 0);
   border: 0px;
+  z-index: 2;
 }
 #button02 {
   height: 40px;
   width: 140px;
   position: absolute;
   left: 1425px;
-  top: 65px;
+  top: 55px;
   font-size: 20px;
   color: #fefefe;
   background: rgba(0, 0, 0, 0);
   border: 0px;
+  z-index: 2;
 }
 #button03 {
   height: 40px;
   width: 140px;
   position: absolute;
   left: 1650px;
-  top: 65px;
+  top: 55px;
   font-size: 20px;
   color: #fefefe;
   background: rgba(0, 0, 0, 0);
   border: 0px;
+  z-index: 2;
 }
 </style>
 
 
+<style>
+#carousel_Map .el-carousel {
+  position: absolute;
+  width: 1920px;
+  height: 812px;
+  top: 125px;
+  left: 0px;
+  z-index: 0;
+}
+#carousel_Map .el-carousel__arrow.el-carousel__arrow--left {
+  position: absolute;
+  top: 405px;
+  width: 80px;
+  height: 80px;
+  background-color: rgb(240, 137, 137);
+  opacity: 0.5;
+  font-size: 30px;
+}
+#carousel_Map .el-carousel__arrow.el-carousel__arrow--right {
+  position: absolute;
+  top: 405px;
+  width: 80px;
+  height: 80px;
+  background-color: rgb(240, 137, 137);
+  opacity: 0.5;
+  font-size: 30px;
+}
+#carousel_Map .el-carousel__arrow.el-carousel__arrow--left:hover {
+  opacity: 0.7;
+}
+#carousel_Map .el-carousel__arrow.el-carousel__arrow--right:hover {
+  opacity: 0.7;
+}
+#carousel_Map .el-carousel__item:nth-child(2n) {
+  background-color: #99a9bf;
+  height: 812px;
+}
+#carousel_Map .el-carousel__button {
+  width: 100px;
+  height: 8px;
+}
+#carousel_Map .el-carousel__item:nth-child(2n + 1) {
+  background-color: #d3dce6;
+  height: 812px;
+}
+</style>
+
+
+
+
+<style>
+#knowledge_Ethics .el-carousel {
+  position: absolute;
+  width: 1920px;
+  height: 812px;
+  top: 125px;
+  left: 0px;
+  z-index: 0;
+}
+#knowledge_Ethics #knowledge_Ethics_Getback {
+  border-radius: 100px;
+  position: absolute;
+  top: 405px;
+  width: 80px;
+  height: 80px;
+  left: 50px;
+  background-color: rgb(240, 137, 137);
+  opacity: 0.5;
+  font-size: 30px;
+  z-index: 3;
+}
+#knowledge_Ethics .el-carousel__arrow.el-carousel__arrow--right {
+  position: absolute;
+  top: 405px;
+  width: 80px;
+  height: 80px;
+  left: 50px;
+  background-color: rgb(240, 137, 137);
+  opacity: 0.5;
+  font-size: 30px;
+  display: none;
+}
+#knowledge_Ethics .el-carousel__arrow.el-carousel__arrow--right:hover {
+  opacity: 0.7;
+  display: none;
+}
+#knowledge_Ethics .el-carousel__arrow.el-carousel__arrow--left {
+  display: none;
+}
+#knowledge_Ethics .el-carousel__item:nth-child(2n) {
+  background-color: #99a9bf;
+  height: 812px;
+}
+#knowledge_Ethics .el-carousel__button {
+  width: 100px;
+  height: 8px;
+}
+#knowledge_Ethics .el-carousel__item:nth-child(2n + 1) {
+  background-color: #d3dce6;
+  height: 812px;
+}
+</style>
