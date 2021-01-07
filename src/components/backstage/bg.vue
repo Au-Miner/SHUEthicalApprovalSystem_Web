b748c6695f9edf244b34fd20d6fbf80c6dccb01f
yuban00018 <caozhuowen2013@outlook.com>
2021-01-07 18:34:24 +0800

    backstage

---
+++ src/components/backstage/bg.vue
@@ -0,0 +1,88 @@
+
+<template>
+  <div id="backstage_bg">
+    <img id="shu_badge" src="../../assets/shu_pic.png" />
+    <div id="pureblue_bg"></div>
+    <div id="lightblue_sidebar">
+        <el-button type="primary" id="tutorial">申报指南</el-button><br>
+        <el-button type="primary" id="apply">申请</el-button><br>
+        <el-button type="primary" id="myapplies">我的申请</el-button><br>
+        <el-button type="primary" id="approve">项目审批</el-button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "backstage_bg",
+  data() {
+    return {};
+  }
+};
+
+</script>
+
+
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+#pureblue_bg {
+  width: 1920px;
+  height: 70px;
+  background-image: linear-gradient(#245086);
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  z-index: -1;
+  /* display: none; */
+}
+#shu_badge {
+  height: 86px;
+  width: 216px;
+  position: absolute;
+  top: 0px;
+  left: 0px;
+}
+#lightblue_sidebar{
+    height:1080px;
+    width:140px;
+    background-image: linear-gradient(#BFE1FD);
+    position:absolute;
+    top:70px;
+    left:0px;
+    visibility:visible;
+    z-index:1;
+    border-right:3px solid;
+    border-right-color:#245086
+}
+button{
+    height:30px;
+    width:140px;
+    background-image: linear-gradient(#769DC5);
+    border-radius:0px;
+    border:none;
+}
+button#tutorial{
+    height:30px;
+    width:140px;
+    background-image: linear-gradient(#245086,#769DC5);
+    border-radius:0px;
+    border:none;
+}
+button#tutorial:hover
+{
+    height:30px;
+    width:140px;
+    background-image: linear-gradient(#66B1FF);
+    border-radius:0px;
+    border:none;
+}
+button#tutorial:active
+{
+    height:30px;
+    width:140px;
+    background-image: linear-gradient(#3A8EE6);
+    border-radius:0px;
+    border:none;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import Title_Start01 from '@/components/Title_Start01'
+import bg from '@/components/backstage/bg'
 
 Vue.use(Router)
 
@@ -12,6 +13,12 @@ export default new Router({
       name: 'HelloWorld',
       // component: HelloWorld,
       component: Title_Start01
+    },
+    {
+      path: '/bg',
+      name: 'bg',
+      // component: HelloWorld,
+      component: bg
     }
   ]
 })
