785e78811869aeab1ee2cb062f783fd8048ed0f1
Au-Miner <358671982@qq.com>
2021-01-07 18:35:09 +0800

    wqlsb

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-import Title_Start01 from '@/components/Title_Start01'
+import Homepage from '@/components/Homepage'
 
 Vue.use(Router)
 
@@ -9,9 +8,8 @@ export default new Router({
   routes: [
     {
       path: '',
-      name: 'HelloWorld',
-      // component: HelloWorld,
-      component: Title_Start01
+      name: 'Homepage',
+      component: Homepage
     }
   ]
 })
