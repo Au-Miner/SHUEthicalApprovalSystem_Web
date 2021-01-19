366c6812b1f59b39e1508eb0745d91c09951eebc
Zhuowen Cao <caozhuowen2013@outlook.com>
2021-01-19 21:29:27 +0800

    表单模板

--- src/components/backstage/myapplications/myapplications.vue
+++ src/components/backstage/myapplications/myapplications.vue
@@ -1,12 +1,82 @@
 <template>
-<p style="color: red; left:25px; position:absolute;">我的申请</p>
+  <div>
+    <template>
+      <el-table
+        :data="information.userApplicationList"
+        style="width: 95%"
+        :row-class-name="ApplicationList"
+      >
+        <el-table-column width="150" fixed prop="id" label="项目编号">
+        </el-table-column>
+        <el-table-column width="200" prop="ordernum" label="ordernum">
+        </el-table-column>
+        <el-table-column width="200" fixed prop="name" label="项目名称">
+        </el-table-column>
+        <el-table-column width="200" prop="userId" label="用户Id">
+        </el-table-column>
+        <el-table-column width="200" prop="creationTime" label="创建时间">
+        </el-table-column>
+        <el-table-column width="200" prop="beginTime" label="开始时间">
+        </el-table-column>
+        <el-table-column width="200" prop="type" label="类型">
+        </el-table-column>
+        <el-table-column width="200" prop="status" label="状态">
+        </el-table-column>
+        <el-table-column width="150" fixed="right" label="操作">
+          <template slot-scope="scope">
+            <el-button
+              size="mini"
+              @click="">编辑
+            </el-button>
+            <el-button
+              size="mini"
+              type="danger"
+              @click="">删除
+            </el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+    </template>
+  </div>
+</template>
+</div>
 </template>
 
 <script>
+import axios from "axios";
 export default {
   name: "myapplications",
   data() {
-    return {};
+    return {
+      information: "",
+    };
+  },
+  mounted() {
+    this.load();
+  },
+  methods: {
+    ApplicationList({ row, rowIndex }) {
+      if (rowIndex === 1) {
+        return "warning-row";
+      } else if (rowIndex === 3) {
+        return "success-row";
+      }
+      return "";
+    },
+    load: function () {
+      axios({
+        url: "/user/applicationList",
+        method: "get",
+      })
+        .then((res) => {
+          if (res.data.code === 200) {
+            this.information = res.data.data;
+          } else this.information = res.data.code;
+        })
+        .catch(() => {
+          console.log("error occur");
+        });
+    },
   },
 };
 </script>
