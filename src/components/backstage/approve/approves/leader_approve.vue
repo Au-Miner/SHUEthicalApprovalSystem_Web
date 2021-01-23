83e9a0dd8731cd9848f4beb4cd63092815557ea3
yuban00018 <caozhuowen2013@outlook.com>
2021-01-23 23:37:24 +0800

    展开式表格以及信息更新模板

--- src/components/backstage/approve/approves/leader_approve.vue
+++ src/components/backstage/approve/approves/leader_approve.vue
@@ -1,12 +1,74 @@
 <template>
-<p style="color: red; left:25px; position:absolute;">领导</p>
+<div id="info">
+   <el-table
+        :data="information.auditResList"
+        style="width: 95%;height: 100%;"
+        id="list"
+      >
+        <el-table-column width="150" fixed prop="id" label="项目编号">
+        </el-table-column>
+        <el-table-column width="200" fixed prop="name" label="项目名称">
+        </el-table-column>
+        <el-table-column width="200" prop="userId" label="用户Id">
+        </el-table-column>
+        <el-table-column width="200" prop="creationTime" label="创建时间">
+        </el-table-column>
+        <el-table-column width="200" prop="type" label="类型">
+        </el-table-column>
+        <el-table-column width="200" prop="status" label="状态">
+        </el-table-column>
+        <el-table-column width="150" fixed="right" label="操作">
+          <template slot-scope="scope">
+            <el-button
+              size="mini"
+              @click="">编辑
+            </el-button>
+            <el-button
+              size="mini"
+              type="danger"
+              @click="">删除
+            </el-button>
+          </template>
+        </el-table-column>
+   </el-table>
+</div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
   name: "leader_approve",
   data() {
-    return {};
+    return {
+        information:''
+    };
+  },
+  mounted(){
+      this.load();
+  },
+  methods: {
+    load: function () {
+      axios({
+        url: "/leader/auditSet",
+        method: "get",
+      })
+        .then((res) => {
+          if (res.data.code === 200) {
+            this.information = res.data.data;
+          } else this.information = res.data.code;
+        })
+        .catch(() => {
+          console.log("error occur");
+        });
+    },
+    test_post() {},
   },
 };
 </script>
+
+<style scoped>
+#list{
+  left:25px;
+  top: 25px;
+}
+</style>
--- src/components/backstage/approve/approves/secretary_approve.vue
+++ src/components/backstage/approve/approves/secretary_approve.vue
@@ -1,12 +1,98 @@
 <template>
-<p style="color: red; left:25px; position:absolute;">学院秘书</p>
+  <div id="info">
+    <el-table
+      ref="multipleTable"
+      :data="information.auditResList"
+      style="width: 95%; height: 100%"
+      id="list"
+    >
+      <el-table-column type="expand">
+        <template slot-scope="props">
+          <el-form label-position="left" inline class="demo-table-expand">
+            <el-form-item label="领导单位">
+              <span>{{ props.row.leaderAgent }}</span>
+            </el-form-item>
+          </el-form>
+        </template>
+      </el-table-column>
+
+      <el-table-column width="150" fixed prop="id" label="项目编号">
+      </el-table-column>
+      <el-table-column width="200" fixed prop="name" label="项目名称">
+      </el-table-column>
+      <el-table-column width="200" prop="userId" label="用户Id">
+      </el-table-column>
+      <el-table-column width="200" prop="creationTime" label="创建时间">
+      </el-table-column>
+      <el-table-column width="200" prop="type" label="类型"> </el-table-column>
+      <el-table-column width="200" prop="status" label="状态">
+      </el-table-column>
+      <el-table-column width="150" fixed="right" label="操作">
+        <template slot-scope="scope">
+          <el-button size="mini" type="info" @click="post(scope)"
+            >详情</el-button
+          >
+          <el-button size="mini" type="success" @click="">批准 </el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+  </div>
 </template>
 
 <script>
+import axios from "axios";
 export default {
   name: "secretary_approve",
   data() {
-    return {};
+    return {
+      information: "",
+    };
+  },
+  mounted() {
+    this.load();
+  },
+  methods: {
+      print:function(sth){console.log(sth)},
+    post: function (scope) {
+      axios({
+        method: "get",
+        url: "/user/applicationInfo?applicationId=" + scope.row.id,
+        data: {},
+      })
+        .then((res) => {
+          if (res.data.code === 200) {
+            console.log(res.data.data);
+            scope.row.leaderAgent=res.data.data.leaderAgent;
+            this.$refs.multipleTable.toggleRowExpansion(scope.row,true);
+            console.log(scope.$index);
+          } else alert(res.data.code);
+        })
+        .catch(() => {
+          alert("error");
+        });
+    },
+    load: function () {
+      axios({
+        url: "/secretary/auditSet",
+        method: "get",
+      })
+        .then((res) => {
+          if (res.data.code === 200) {
+            this.information = res.data.data;
+          } else this.information = res.data.code;
+        })
+        .catch(() => {
+          console.log("error occur");
+        });
+    },
+    test_post() {},
   },
 };
 </script>
+
+<style scoped>
+#list {
+  left: 25px;
+  top: 25px;
+}
+</style>
