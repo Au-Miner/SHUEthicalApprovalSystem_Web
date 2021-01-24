8f8b94ad86664a75a1dd9b61ee5a86ec43704580
yuban00018 <caozhuowen2013@outlook.com>
2021-01-24 23:43:10 +0800

    审批详细信息

--- src/components/backstage/approve/approves/secretary_approve.vue
+++ src/components/backstage/approve/approves/secretary_approve.vue
@@ -9,8 +9,38 @@
       <el-table-column type="expand">
         <template slot-scope="props">
           <el-form inline class="table-expand">
-            <el-form-item label="领导单位">
+            <el-form-item label="项目单位">
+              <span>{{ props.row.institution }}</span>
+            </el-form-item><br />
+            <el-form-item label="学院秘书经办人">
+              <span>{{ props.row.secretaryAgent }}</span>
+            </el-form-item>
+            <el-form-item label="部门领导经办人">
               <span>{{ props.row.leaderAgent }}</span>
+            </el-form-item>
+            <el-form-item label="委员长经办人">
+              <span>{{ props.row.chairmanAgent }}</span>
+            </el-form-item>
+            <el-form-item label="委员经办人">
+              <span>{{ props.row.memberAgent }}</span>
+            </el-form-item><br />
+            <el-form-item label="预定的起止时间">
+              <span>{{ props.row.scheduleTime }}</span>
+            </el-form-item>
+            <el-form-item label="申请创建时间">
+              <span>{{ props.row.creationTime }}</span>
+            </el-form-item><br />
+            <el-form-item label="申请同意时间">
+              <span>{{ props.row.beginTime }}</span>
+            </el-form-item>
+            <el-form-item label="执行期">
+              <span>{{ props.row.executionTime }}</span>
+            </el-form-item>
+            <el-form-item label="结束时间">
+              <span>{{ props.row.endTime }}</span>
+            </el-form-item><br />
+            <el-form-item label="状态">
+              <span>{{ props.row.status }}</span>
             </el-form-item>
             <br />
             <el-form-item label="修改意见">
@@ -82,6 +112,16 @@ export default {
         .then(res => {
           if (res.data.code === 200) {
             row.leaderAgent = res.data.data.leaderAgent;
+            row.institution = res.data.data.institution;
+            row.secretaryAgent = res.data.data.secretaryAgent;
+            row.chairmanAgent = res.data.data.chairmanAgent;
+            row.memberAgent = res.data.data.memberAgent;
+            row.scheduleTime = res.data.data.scheduleTime;
+            row.creationTime = res.data.data.creationTime;
+            row.beginTime = res.data.data.beginTime;
+            row.executionTime = res.data.data.executionTime;
+            row.endTime = res.data.data.endTime;
+            row.status = res.data.data.status;
             this.$refs.multipleTable.toggleRowExpansion(row, true);
           } else alert(res.data.code);
         })
