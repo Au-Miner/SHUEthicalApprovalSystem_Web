fb86aba4ff5ad47574f0a92e0a55050e27b2e106
yuban00018 <caozhuowen2013@outlook.com>
2021-02-03 17:35:35 +0800

    tutorial

--- src/components/backstage/tutorial/tutorial.vue
+++ src/components/backstage/tutorial/tutorial.vue
@@ -1,17 +1,36 @@
 <template>
-  <div>
+  <div id="whiteBG">
     <!--<div id="tutorial-button01-father">
       <div id="tutorial-button01">{{tutorial_button01_data}}</div>
     </div>-->
-    <div id="tutorial-title01">{{tutorial_title01_data}}</div>
+    <h1 id="title">申报指南</h1>
+    <!--<div id="tutorial-title01">{{tutorial_title01_data}}</div>-->
     <!--<img id="tutorial_img01" v-bind:src="tutorial_img01_data.src"/>-->
-    <div id="steps">
-  <el-steps direction="vertical" active="3" space="50px">
-    <el-step title="步骤 1"></el-step>
-    <el-step title="步骤 2"></el-step>
-    <el-step title="步骤 3" description="这是一段很长很长很长的描述性文字"></el-step>
+  <h2 style="position:relative;margin: 0 auto;top:150px;">申请创建</h2>
+  <div id="steps1">
+  <el-steps :active="3" align-center :space="200">
+    <el-step icon="el-icon-edit" title="申请人创建申请" description="文章/项目/其他申请"></el-step>
+    <el-step icon="el-icon-upload" title="申请人提交申请"></el-step>
+    <el-step icon="el-icon-s-check" title="进入审核流程"></el-step>
   </el-steps>
-</div>
+  </div>
+  <h2 style="position:relative;margin: 0 auto;top:225px;">审核</h2>
+  <div id="steps2">
+  <el-steps :active="4" align-center :space="200">
+    <el-step icon="el-icon-s-check" title="科研秘书初审"></el-step>
+    <el-step icon="el-icon-s-check" title="二级学院领导复审"></el-step>
+    <el-step icon="el-icon-s-check" title="科技伦理委员会审核"></el-step>
+    <el-step icon="el-icon-document-checked" title="委员会出具审查同意书"></el-step>
+  </el-steps>
+  </div>
+  <h2 style="position:relative;margin: 0 auto;top:300px;">完成项目</h2>
+  <div id="steps3">
+  <el-steps :active="3" align-center :space="200">
+    <el-step icon="el-icon-upload" title="提交伦理执行情况表格"></el-step>
+    <el-step icon="el-icon-upload" title="提交伦理工作总结"></el-step>
+    <el-step icon="el-icon-upload" title="提交跟踪情况表"></el-step>
+  </el-steps>
+  </div>
   </div>
 </template>
 
@@ -60,9 +79,34 @@ export default {
   width: 55%;
   height: 85%;
 }
-#steps{
+#steps1{
+  position:relative;
+  top:180px;
+  margin: 0 auto;
+  width: 600px;
+}
+#steps2{
+  position: relative;
+  margin: 0 auto;
+  top:250px;
+  width:800px;
+}
+#steps3{
+  position: relative;
+  top:330px;
+  margin: 0 auto;
+  width: 600px;
+}
+#whiteBG{
+  background-image: linear-gradient(white);
+  width: 100%;
+  height:100%;
   position:absolute;
-  left:5%;
-  top:10%;
+}
+#title
+{
+  position: relative;
+  top:50px;
+  margin: 0 auto;
 }
 </style>
